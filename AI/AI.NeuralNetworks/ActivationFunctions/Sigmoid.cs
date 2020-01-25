9e2a6d3d49072f03ff06e531fb8771edf8b7c9f1
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 21:24:36 +0300

    Функция-активации Сигмоид

---
+++ AI/AI.NeuralNetworks/ActivationFunctions/Sigmoid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntennaAI.AI.NeuralNetworks.ActivationFunctions
+{
+    /// <summary>Логистическая функция (Сигмоид)</summary>
+    public class Sigmoid : DiffSimplifiedActivationFunction
+    {
+        public static double Activation(double x) => 1 / (1 + Math.Exp(-x));
+
+        public static double DiffActivation(double u) => u * (1 - u);
+
+        public override double Value(double x) => Activation(x);
+
+        public override double DiffValue(double x) => DiffFunc(Value(x));
+
+        public override double DiffFunc(double u) => DiffActivation(u);
+
+        public double Inverse(double u) => -Math.Log(1 / u - 1);
+    }
+}
