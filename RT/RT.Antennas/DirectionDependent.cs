20ba43ce289fac77f382765b721404297ff5605d
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 18:57:56 +0300

    Элемент Гюйгенса и симметричный вибратор

---
+++ RT/RT.Antennas/DirectionDependent.cs
@@ -0,0 +1,12 @@
+﻿using AntennaAI.RT.Base.Signals;
+
+namespace AntennaAI.RT.Antennas
+{
+    /// <summary>Антенна с направленными свойствами</summary>
+    public abstract class DirectionDependent : Antenna
+    {
+        /// <summary>Амплитуда сигнала на выходе антенны пропорциональна амплитуде диаграммы направленности</summary>
+        public override Signal GetSignal(Signal s, double Theta, double Phi) 
+            => t => s(t) * Pattern(Theta, Phi).Magnitude;
+    }
+}
