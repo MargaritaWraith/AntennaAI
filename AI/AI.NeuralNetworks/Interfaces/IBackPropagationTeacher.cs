f78bb6712c5c8927338f95aa4607cec61e130a62
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 21:33:25 +0300

    Интерфейс учителя нейроной сети методом обратного распространения ошибки

---
+++ AI/AI.NeuralNetworks/Interfaces/IBackPropagationTeacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntennaAI.AI.NeuralNetworks.Interfaces
+{
+    /// <summary>Учитель нейронной сети методом обратного распространения ошибки</summary>
+    public interface IBackPropagationTeacher : INetworkTeacher
+    {
+        /// <summary>Коэффициент скорости обучения (0..1)</summary>
+        double Rho { get; set; }
+        /// <summary>Инерционность процесса обучения [0..1)</summary>
+        double InertialFactor { get; set; }
+    }
+}
