d35bafcf9528c55bfec5cde713ceae068ab00fdf
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 22:51:13 +0300

    Реализация процесса обучения нейронной сети эпохами на наборе заданных примеров.

--- AI/AI.NeuralNetworks/NeuralNetworkExtensions.cs
+++ AI/AI.NeuralNetworks/NeuralNetworkExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AntennaAI.AI.NeuralNetworks.Interfaces;
 
 namespace AntennaAI.AI.NeuralNetworks
@@ -40,6 +42,63 @@ namespace AntennaAI.AI.NeuralNetworks
                 var delta = ExpectedOutput[i] - Output[i];
                 Error[i] = 0.5 * delta * delta;
             }
+        }
+
+        /// <summary>Выполнить обучение нейронной сети (одну эпоху) по заданному набору обучающих примеров</summary>
+        /// <param name="Teacher">Обучаемая нейронная сеть</param>
+        /// <param name="Examples">Набор обучающих примеров</param>
+        /// <returns>Результат выполнения обучения за эпоху</returns>
+        public static Epoch Teach(this INetworkTeacher Teacher, params Example[] Examples)
+        {
+            if (Teacher is null) throw new ArgumentNullException(nameof(Teacher));
+            if (Examples is null) throw new ArgumentNullException(nameof(Examples));
+            if (Teacher.Network.InputsCount == 0) throw new InvalidOperationException("Сеть не имеет входов");
+            if (Teacher.Network.OutputsCount == 0) throw new InvalidOperationException("Сеть не имеет выходов");
+
+            var inputs_count = Teacher.Network.InputsCount;
+            var outputs_count = Teacher.Network.OutputsCount;
+            for (var i = 0; i < Examples.Length; i++)
+            {
+                var example = Examples[i];
+                // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                if (example is null)
+                    // ReSharper disable once HeuristicUnreachableCode
+                    throw new InvalidOperationException($"Обучающий пример с индексом {i} отсутствует");
+                if (example.Input.Length != inputs_count)
+                    throw new InvalidOperationException($"Длина входного вектора примера №{i} ({example.Input.Length}) не равна количеству входов сети ({inputs_count})");
+                if (example.ExpectedOutput.Length != outputs_count)
+                    throw new InvalidOperationException($"Длина вектора ожидаемого результата №{i} ({example.ExpectedOutput.Length}) не равна количеству выходов сети ({outputs_count})");
+            }
+
+            return Teach(Teacher, (IEnumerable<Example>)Examples);
+        }
+
+        /// <summary>Обучение нейронной сети на заданном перечислении примеров эпохи</summary>
+        /// <param name="Teacher">Учитель нейронной сети, по средствам которого осуществляется процесс обучения</param>
+        /// <param name="Examples">Перечисление примеров для одной эпохи обучения</param>
+        /// <returns>Результат обучения нейронной сети за эпоху</returns>
+        private static Epoch Teach(this INetworkTeacher Teacher, IEnumerable<Example> Examples)
+        {
+            var outputs_count = Teacher.Network.OutputsCount;
+            var examples = Examples as Example[] ?? Examples.ToArray();
+            var results = new TeachResult[examples.Length];
+            var max_error = 0d;
+            var avg_error = 0d;
+            var output = new double[outputs_count];
+            for (var i = 0; i < examples.Length; i++)
+            {
+                var example = examples[i];
+                var example_input = example.Input;
+                var example_expected_output = example.ExpectedOutput;
+                var error = Teacher.Teach(example_input, output, example_expected_output);
+                if (error > max_error) max_error = error;
+                avg_error += error;
+                if (double.IsNaN(error)) throw new InvalidOperationException("Ошибка сети является \"не числом\" - возможно сеть нестабвльна.");
+                if (double.IsInfinity(error)) throw new InvalidOperationException("Ошибка сети является \"бесконечностью\" - возможно сеть нестабильна.");
+                results[i] = new TeachResult(example, output, error);
+            }
+
+            return new Epoch(results, max_error, avg_error / examples.Length);
         }
     }
 }
