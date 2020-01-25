1303b1c2498e6558f01e3b56421faa0f9060200e
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 21:24:21 +0300

    Упрощённо-дифференцируемые функции активации

---
+++ AI/AI.NeuralNetworks/ActivationFunctions/DiffSimplifiedActivationFunction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AntennaAI.AI.NeuralNetworks.ActivationFunctions
+{
+    /// <summary>Активационная функция с упрощённой производной</summary>
+    public abstract class DiffSimplifiedActivationFunction : ActivationFunction
+    {
+        /// <summary>Производная функции, выраженная через значение самой функции</summary>
+        /// <param name="u">Значение функции по которому рассчитывается значение производной</param>
+        /// <returns>Значение производной функции</returns>
+        public abstract double DiffFunc(double u);
+    }
+}
