a0733284da7e41ddb9bfb7c0d6c4a29ce307e814
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 21:51:01 +0300

    Линейрая функция-автивации

--- AI/AI.NeuralNetworks/ActivationFunctions/ActivationFunction.cs
+++ AI/AI.NeuralNetworks/ActivationFunctions/ActivationFunction.cs
@@ -7,6 +7,14 @@ namespace AntennaAI.AI.NeuralNetworks.ActivationFunctions
     /// <summary>Функция активации</summary>
     public abstract class ActivationFunction
     {
+        /// <summary>Линейная функция</summary>
+        public static Linear Linear => new Linear();
+
+        /// <summary>Линейная функция с параметрами</summary>
+        /// <param name="K">Производная</param>
+        /// <param name="B">Смещение</param>
+        public static Linear GetLinear(double K, double B = 0) => new Linear(K, B);
+
         /// <summary>Значение функции активации</summary>
         /// <param name="x">Взвешенная сумма входов нейронов</param>
         /// <returns>Значение выхода нейрона</returns>
