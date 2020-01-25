8a6d6725a93c695922a7a368e6b75847906ad1b5
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 18:39:52 +0300

    Всенаправленная антенна

---
+++ RT/RT.Antennas/Uniform.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using AntennaAI.RT.Base.Signals;
+
+namespace AntennaAI.RT.Antennas
+{
+    /// <summary>Всенаправленная антенна</summary>
+    public class Uniform : Antenna
+    {
+        /// <summary>Значение диаграммы направленности для любого значения угла = 1</summary>
+        public override Complex Pattern(double Theta, double Phi) => 1;
+
+        /// <summary>Сигнал на выходе антенны соответствует сигналу волны</summary>
+        public override Signal GetSignal(Signal s, double Theta, double Phi) => s;
+    }
+}
