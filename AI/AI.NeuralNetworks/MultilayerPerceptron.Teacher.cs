db7eff3516f80248420fe8ad6348ed8db3e34803
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 20:57:14 +0300

    Заготовка нейронной сети

---
+++ AI/AI.NeuralNetworks/MultilayerPerceptron.Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AntennaAI.AI.NeuralNetworks.Interfaces;
+
+namespace AntennaAI.AI.NeuralNetworks
+{
+    public partial class MultilayerPerceptron
+    {
+        public INetworkTeacher CreateTeacher() => throw new NotImplementedException();
+
+        public TNetworkTeacher CreateTeacher<TNetworkTeacher>(Action<TNetworkTeacher> Configurator = null) where TNetworkTeacher : class, INetworkTeacher => throw new NotImplementedException();
+    }
+}
---
+++ AI/AI.NeuralNetworks/MultilayerPerceptron.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using AntennaAI.AI.NeuralNetworks.Interfaces;
+
+namespace AntennaAI.AI.NeuralNetworks
+{
+    /// <summary>Многослойный персептрон</summary>
+    public partial class MultilayerPerceptron : ITeachableNeuralNetwork
+    {
+        /* --------------------------------------------------------------------------------------------- */
+
+        #region Поля 
+
+        /// <summary>Матрицы коэффициентов передачи слоёв (номер строки - номер нейрона; номер столбца - номер входа нейрона; последний столбец - смещение нейрона)</summary>
+        protected readonly double[][,] _Layers;
+
+        /// <summary>Массив смещений нейронов в слоях (первый индекс - номер слоя; второй - номер нейрона в слое)</summary>
+        protected readonly double[][] _Offsets;
+
+        /// <summary>Массив весовых коэффициентов смещений нейронов в слоях (первый индекс - номер слоя; второй - номер нейрона в слое)</summary>
+        protected readonly double[][] _OffsetsWeights;
+
+        /// <summary>Массив выходов скрытых слоёв</summary>
+        protected readonly double[][] _Outputs;
+
+        #endregion
+
+        /* --------------------------------------------------------------------------------------------- */
+
+        #region Свойства
+
+        public int InputsCount => _Layers[0].GetLength(1);
+
+        public int OutputsCount => _Layers[_Layers.Length - 1].GetLength(0);
+
+        #endregion
+
+        /* --------------------------------------------------------------------------------------------- */
+
+        #region Конструкторы
+
+        public MultilayerPerceptron() { }
+
+        #endregion
+
+        /* --------------------------------------------------------------------------------------------- */
+
+        #region Методы
+        
+        public void Process(Span<double> Input, Span<double> Output) => throw new NotImplementedException(); 
+
+        #endregion
+
+        /* --------------------------------------------------------------------------------------------- */
+    }
+}
