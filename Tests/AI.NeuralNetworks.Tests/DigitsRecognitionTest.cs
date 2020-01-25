781fb527c0aacde80c7e4dcf2219e20a8cb70cd1
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 23:17:25 +0300

    Множественный тест на наборе данных в задаче распознавания растровых символов

--- Tests/AI.NeuralNetworks.Tests/DigitsRecognitionTest.cs
+++ Tests/AI.NeuralNetworks.Tests/DigitsRecognitionTest.cs
@@ -134,6 +134,30 @@ namespace AI.NeuralNetworks.Tests
             return result;
         }
 
+        private static int[] AddBinaryNoise([NotNull] int[] Data, int DistortionsCount, Random rnd = null)
+        {
+            switch (DistortionsCount)
+            {
+                case 0: return Data;
+                case 1: return AddBinaryNoise(Data, rnd);
+            }
+
+            var result = (int[])Data.Clone();
+            if (rnd is null) rnd = new Random();
+
+            if (DistortionsCount < result.Length)
+            {
+                var indexes = Enumerable.Range(0, Data.Length).ToArray().Mix(rnd).Take(DistortionsCount).ToArray();
+                foreach (var index in indexes)
+                    result[index] = Data[index] > 0 ? 0 : 1;
+            }
+            else
+                foreach (var index in rnd.SequenceInt(0, Data.Length, DistortionsCount))
+                    result[index] = Data[index] > 0 ? 0 : 1;
+
+            return result;
+        }
+
         private static int[][] GetDigitSymbolsImages(Dictionary<char, int[]> Symbols = null) => (Symbols ?? __Symbols)
            .Where(v => char.IsDigit(v.Key))
            .ToDictionary(v => (int)char.GetNumericValue(v.Key), v => v.Value)
@@ -226,6 +250,32 @@ namespace AI.NeuralNetworks.Tests
             Debug.WriteLine("Средняя ошибка распознавания символов составила {0:p2}", average_error);
             const double error_threshold = 0.15;
             Assert.That.Value(average_error).LessThan(error_threshold);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 0.25)]
+        [DataRow(3, 0.35)]
+        public void DigitsRecognition_With_Distortions_Test(int DistortionsCount, double ErrorThreshold)
+        {
+            var processor = GetProcessor().Processor;
+
+            var rnd = new Random();
+            var chars = GetDigitSymbolsImages();
+            var results = chars.Select(processor.Process).ToArray();
+
+            var diff_results = new int[1000][];
+            for (var i = 0; i < 1000; i++)
+            {
+                var noisy_chars = chars.Select(c => AddBinaryNoise(c, DistortionsCount, rnd)).ToArray();
+                var noisy_results = noisy_chars.Select(processor.Process).ToArray();
+
+                diff_results[i] = GetDiffIndexes(results, noisy_results);
+            }
+
+            var average_error = diff_results.Average(errors => errors.Length);
+            Debug.WriteLine("Средняя ошибка распознавания символов при {0} искажениях составила {1:p2}",
+                DistortionsCount, average_error);
+            Assert.That.Value(average_error).LessThan(ErrorThreshold);
         }
     }
 }
