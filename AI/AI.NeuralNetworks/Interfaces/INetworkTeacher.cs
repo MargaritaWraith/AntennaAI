f88780912feb4c7b3ec22f648a79b3828bc52a5a
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 20:49:22 +0300

    Учитель нейронной сети

---
+++ AI/AI.NeuralNetworks/Interfaces/INetworkTeacher.cs
@@ -0,0 +1,16 @@
+﻿namespace AntennaAI.AI.NeuralNetworks.Interfaces
+{
+    /// <summary>Учитель нейронной сети</summary>
+    public interface INetworkTeacher
+    {
+        /// <summary>Обучаемая сеть</summary>
+        INeuralNetwork Network { get; }
+
+        /// <summary>Обучение сети методом обратного распространения ошибки</summary>
+        /// <param name="Input">Массив входа</param>
+        /// <param name="Output">Массив выхода</param>
+        /// <param name="Expected">Ожидаемое значение на выходе сети</param>
+        /// <returns>Среднеквадратичная ошибка обучения</returns>
+        double Teach(double[] Input, double[] Output, double[] Expected);
+    }
+}
