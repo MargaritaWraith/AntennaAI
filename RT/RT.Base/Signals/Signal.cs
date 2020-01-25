ee876a7c474d47bd8d96687d7a49c38993c6358e
MargaritaWraith <kondratieff89@ya.ru>
2020-01-25 18:35:19 +0300

    Базовый класс антенны

---
+++ RT/RT.Base/Signals/Signal.cs
@@ -0,0 +1,7 @@
+﻿namespace AntennaAI.RT.Base.Signals
+{
+    /// <summary>Аналоговый сигнал, как непрерывная функция времени</summary>
+    /// <param name="Time">Значение времени (в секундах)</param>
+    /// <returns>Значение амплитуды сигнала в заданный момент времени</returns>
+    public delegate double Signal(double Time);
+}
